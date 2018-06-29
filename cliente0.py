5bc2c73101e1b371b53173fb7930b1e292fb0592
Giovani <giovanibossoni@hotmail.com>
2018-06-28 22:25:10 -0300

    implementado certo a parte das passagens

--- cliente0.py
+++ cliente0.py
@@ -17,9 +17,10 @@ entrada = -1
 while entrada != 0:
     print('''
     [1]Listar Passagens
-    [2]Comprar Passagens
-    [3]Listar Hotel
-    [4]Reservar Hotel
+    [2]Filtrar Passagens
+    [3]Comprar Passagens
+    [4]Listar Hotel
+    [5]Reservar Hotel
     [0]SAIR
     ''')
     entrada=int(input('Qual das opcoes deseja?'))
@@ -27,37 +28,59 @@ while entrada != 0:
     if (entrada==1):
         resp = requests.get(url)
         respdec=resp.json()
-        print (len(respdec))
-        print (respdec)
+       ## print (len(respdec))
+       ## print (respdec)
         certo = respdec['dados']
         rang=int (len(certo))
         for i in range (0, rang):
-            print("id:"+ str(certo[i]['id']))
+            print("id:" + str(certo[i]['id']))
             print("data:" + str(certo[i]['data']))
+            print("data:" + str(certo[i]['dataVolta']))
             print("origem:" + str(certo[i]['origem']))
             print("destino:" + str(certo[i]['destino']))
             print("numero:" + str(certo[i]['numero']))
             print("tipo:" + str(certo[i]['tipo']))
-            print("comprada:" + str(certo[i]['comprada']))
             print("-=-=-=-=-=-=-=-")
 
-    elif (entrada==2):
+    if (entrada==2):
+
         data = input("Data da viagem: ")
         origem = input("Origem: ")
         destino = input("Destino: ")
-        numero = input("Poltrona: ")
-        tipo = input("ida ou ida/volta: ")
-        bandeira = input("Bandeira do cartão:")
-        parcelas = input("Parcelas:")
-        idade = input("Idade")
-        r = requests.post(url='http://localhost:8080/passagem/1', json={"cartao": bandeira, "parcela": parcelas, "idade": idade})
-        print (r.status_code)
+        dataVolta = input("Data Volta: ")
+        opcao = input("Opção (ida ou ida e volta): ")
+        parametros = (('data', data), ('origem', origem), ('destino', destino), ('dataVolta',dataVolta),('opcao',opcao))
+        resp = requests.get(url, params=parametros)
+        respdec=resp.json()
+       ## print (len(respdec))
+       ## print (respdec)
+        certo = respdec['dados']
+        rang=int (len(certo))
+        print("-=-=-=-=-=-=-=-")
+        for i in range (0, rang):
+            print("id:"+ str(certo[i]['id']))
+            print("data:" + str(certo[i]['data']))
+            print("data:" + str(certo[i]['dataVolta']))
+            print("origem:" + str(certo[i]['origem']))
+            print("destino:" + str(certo[i]['destino']))
+            print("numero:" + str(certo[i]['numero']))
+            print("tipo:" + str(certo[i]['tipo']))
+            print("-=-=-=-=-=-=-=-")
 
     elif (entrada==3):
+        id = input("Digite o ID da passamgem: ")
+        bandeira = input("Bandeira do cartão:")
+        parcelas = input("Parcelas:")
+        idade = input("Idade: ")
+        qtd = input("Quantidade Pessoas: ")
+        r = requests.post(url='http://localhost:8080/passagem/'+id, json={"cartao": bandeira, "parcela": parcelas, "idade": idade, "numeroPessoas": qtd})
+        print(r.json()["message"])
+
+    elif (entrada==4):
         resp = requests.get(url)
         respdec=resp.json()
-        print (len(respdec))
-        print (respdec)
+     ##   print (len(respdec))
+     ##   print (respdec)
         certo = respdec['dados']
         rang=int (len(certo))
         for i in range (0, rang):
@@ -65,10 +88,9 @@ while entrada != 0:
             print("data:" + str(certo[i]['data']))
             print("local:" + str(certo[i]['local']))
             print("numero:" + str(certo[i]['numero']))
-            print("comprada:" + str(certo[i]['comprada']))
             print("-=-=-=-=-=-=-=-")
 
-    elif (entrada==4):
+    elif (entrada==5):
         datah = input("Data de saida: ")
         quartoh = input("Quantidade de quartos: ")
         bandeirah = input("Bandeira do cartão:")
